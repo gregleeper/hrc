3f6066da6691d4b115459fe588ed750a83a28382
gregleeper <gregleeper19@gmail.com>
2019-02-26 13:31:40 -0600

    second commit

--- src/pages/facilities.js
+++ src/pages/facilities.js
@@ -22,6 +22,7 @@ const Facilities = ({ props }) => {
           "leisure"
         ]}
       />
+      <div />
       <div className="container">
         <h2>Facilities</h2>
         <div className={styles.mainOffice}>
