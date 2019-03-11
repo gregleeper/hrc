c9b97aa2a1cbbb89e4ee807cac71913ee05e3245
gregleeper <gregleeper19@gmail.com>
2019-03-11 10:13:08 -0500

    added content to swim lessons page

---
+++ src/pages/summer/swim-lessons.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class SwimLessons extends Component {
+  state = {};
+  render() {
+    return <div>swim lessons</div>;
+  }
+}
+
+export default SwimLessons;
